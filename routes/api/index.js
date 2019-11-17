0fb4e0923c3405e50c42669af9e5829c26cf7b29
LDibs <laurencedibah@gmail.com>
2019-11-17 14:51:31 +1100

    created method to verify locations

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,18 +1,33 @@
 const dotenv = require('dotenv')
 const express = require('express');
-const router = express.Router();
 const axios = require('axios');
+const fs = require('fs');
 
-router.get('/ping', (request, response) => {
-  response.send('working');
-})
+const router = express.Router();
 
-router.get('/weather', (request, response) => {
+const cityList = fs.readFileSync('./constants/city.list.json');
+const cities = JSON.parse(cityList.toString())
+
+const verifyLocation = (request, response, next) => {
   const city = request.query.city;
   const country = request.query.country;
   if(!city || !country) {
     response.send('Missing params');
   }
+  for (let i = 0; i < cities.length; i++) {
+    console.log(cities[i].name);
+    if (cities[i].country === country) {
+      if(cities[i].name === city) {
+        return next()
+      }
+    }
+  }
+  response.send('Invalid City or Country')
+}
+
+router.get('/weather', verifyLocation, (request, response) => {
+  const city = request.query.city;
+  const country = request.query.country;
   axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.API_KEY}`)
   .then(result => {
     const description = result.data.weather[0].description;
