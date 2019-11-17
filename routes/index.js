ec6b6316747588bb54753ddce828110ff883b2db
LDibs <laurencedibah@gmail.com>
2019-11-17 17:42:19 +1100

    added styling and error handling for front end

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const router = express.Router();
 
 router.use('/api', require('./api'));
+
+router.get('/', (request, response) => {
+    response.sendFile(path.resolve('./views/index.html'));
+})
 
 module.exports = router;
