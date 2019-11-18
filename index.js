53d50321cb6b0a5c7f6acde3bfa7ed375fc81729
LDibs <laurencedibah@gmail.com>
2019-11-19 00:48:46 +1100

    removed unnecessary package

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
 
@@ -9,7 +8,6 @@ const port = process.env.SERVER_PORT || 5000;
 app.listen(port, () => {
   console.log(`listening on port: ${port}`)
   app.use(express.json());
-  app.use(cors());
   app.use('/', require('./routes'));
 });
 
